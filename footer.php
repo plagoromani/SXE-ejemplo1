ee6cc3ecce91a195adb9ef0187e00e077be4e02d
Pablo <plagoromani@danielcastelao.org>
2016-12-11 19:29:15 +0000

    nuevos cambios

--- footer.php
+++ footer.php
@@ -4,7 +4,14 @@
         
  
 <footer class="footer-distributed">
-
+<div id ="direccion">
+	
+		<li>C/Zaragoza</li>
+		<li>Vigo</li>
+		<li>36203</li>
+	<li>Pablite5 ©</li>
+	
+</div>
 
 		</footer>
 	
